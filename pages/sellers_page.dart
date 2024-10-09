5be1e7cff44c23135278fb8e2208a4fefa9a80f6
Zeke142 <dirthaul142@gmail.com>
2024-10-08 17:05:41 -0700

    Create sellers_page.dart

---
+++ pages/sellers_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SellersPage extends StatelessWidget {
+  const SellersPage({super.key});  // Converted 'key' to a super parameter
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sellers Page'),
+      ),
+      body: const Center(
+        child: Text('This is the Sellers Page'),
+      ),
+    );
+  }
+}
