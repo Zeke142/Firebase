64c90076f52e450904bfc4e9dfa916fb65b9f35b
Zeke142 <dirthaul142@gmail.com>
2024-10-08 17:06:31 -0700

    Create transport_page.dart

---
+++ pages/transport_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TransportPage extends StatelessWidget {  // Public class without underscore
+  const TransportPage({super.key});  // Converted 'key' to a super parameter
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transport Page'),
+      ),
+      body: const Center(
+        child: Text('This is the Transport Page'),
+      ),
+    );
+  }
+}
